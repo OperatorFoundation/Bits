79d4d535c48527155b328d59bc97e32eb6be63fa
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2020-06-30 13:59:35 -0500

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Bits",
+    platforms: [
+        .macOS(.v10_15)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -25,5 +28,6 @@ let package = Package(
         .testTarget(
             name: "BitsTests",
             dependencies: ["Bits"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
